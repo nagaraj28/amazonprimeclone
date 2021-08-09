81fda61a081c6dc49ab24a83467cb88fd053e06a
nagaraj rathna <34264078+nagaraj28@users.noreply.github.com>
2021-08-09 19:24:44 +0530

    update:css changes

--- amazonprimeclone/src/container/header.js
+++ amazonprimeclone/src/container/header.js
@@ -39,14 +39,58 @@ export default function HeaderContainer() {
       </Header.SearchContainer>
       <Header.Profile onClick={profileExpand} >
         <div style={{display:"flex"}}>
-        <h3 className="proName" >{profileName}</h3>
+          <span className="drpdwnup">
+          <svg width="30" height="30.532" xmlns="http://www.w3.org/2000/svg">
+ <g>
+  <title>background</title>
+  <rect fill="none" id="canvas_background" height="32.532" width="32" y="-1" x="-1"/>
+ </g>
+ <g>
+  <title>Layer 1</title>
+  <g stroke="null" id="svg_1">
+   <path fill="#f76565" id="svg_2" d="m15.266,0.001c-8.430298,0 -15.266,6.834214 -15.266,15.265s6.835032,15.265 15.266,15.265c8.431639,0 15.266,-6.834214 15.266,-15.265s-6.834362,-15.265 -15.266,-15.265zm0,4.564413c2.789535,0 5.049998,2.261084 5.049998,5.049218c0,2.788805 -2.260462,5.049218 -5.049998,5.049218c-2.788194,0 -5.048657,-2.260414 -5.048657,-5.049218c0,-2.788134 2.260462,-5.049218 5.048657,-5.049218zm-0.003353,21.974492c-2.782159,0 -5.330292,-1.013196 -7.295708,-2.690234c-0.478781,-0.408363 -0.755052,-1.007161 -0.755052,-1.635464c0,-2.827696 2.288626,-5.090792 5.117054,-5.090792l5.875459,0c2.829098,0 5.109007,2.263096 5.109007,5.090792c0,0.628973 -0.27493,1.22643 -0.754382,1.634793c-1.964745,1.677708 -4.513549,2.690905 -7.296378,2.690905z"/>
+  </g>
+  <g id="svg_3"/>
+  <g id="svg_4"/>
+  <g id="svg_5"/>
+  <g id="svg_6"/>
+  <g id="svg_7"/>
+  <g id="svg_8"/>
+  <g id="svg_9"/>
+  <g id="svg_10"/>
+  <g id="svg_11"/>
+  <g id="svg_12"/>
+  <g id="svg_13"/>
+  <g id="svg_14"/>
+  <g id="svg_15"/>
+  <g id="svg_16"/>
+  <g id="svg_17"/>
+ </g>
+</svg>
+          </span>
+        <p className="proName" >{profileName}</p>
         {
-        isProfileExpand?(<span className="drpdwn">
-            <svg width="30" height="17" xmlns="http://www.w3.org/2000/svg"><g> <title>background</title> <rect fill="none" id="canvas_background" y="-1" x="-1"/></g><g> <title>Layer 1</title> <polygon transform="rotate(-180 24,24) " id="svg_1" points="0,12.438 48,12.438 24,35.562 " fill="#cccccc"/> </g></svg>
-            </span>):(<span className="drpdwn">
-            <svg width="30" height="17" xmlns="http://www.w3.org/2000/svg"><g> <title>background</title> <rect fill="none" id="canvas_background" y="-1" x="-1"/></g><g> <title>Layer 1</title> <polygon transform="rotate(-180 24,24) " id="svg_1" points="0,12.438 48,12.438 24,35.562 " fill="#cccccc"/> </g></svg>
+        isProfileExpand?(<span className="drpdwnup">
+                      <svg width="40" height="17" xmlns="http://www.w3.org/2000/svg"><g> <title>background</title> <rect fill="none" id="canvas_background" y="-1" x="-1"/></g><g> <title>Layer 1</title> <polygon transform="rotate(-180 24,24) " id="svg_1" points="0,12.438 48,12.438 24,35.562 " fill="#cccccc"/> </g></svg>
+            </span>):(
 
-                        </span>)
+<span className="drpdwndown">
+<svg width="10" height="10" xmlns="http://www.w3.org/2000/svg">
+ <style type="text/css"></style>
+ <g>
+  <title>background</title>
+  <rect fill="none" id="canvas_background" height="12" width="10" y="-1" x="-1"/>
+ </g>
+ <g>
+  <title>Layer 1</title>
+  <g stroke="null" id="svg_1">
+   <polygon stroke="#b2b2b2" fill="#cccccc" id="svg_2" points="3.4399988651275635,6.900001799644542 8.879997253417969,0 -2,0 3.4399988651275635,6.900001799644542 " class="st0"/>
+  </g>
+ </g>
+</svg>
+</span>
+              
+                        )
 }
        </div>
            
